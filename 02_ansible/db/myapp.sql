873549807ba3d569e35584853c277735720741d6
Pernat1y <1ns@pm.me>
2021-11-29 14:26:18 +0200

    Add database dump

---
+++ 02_ansible/db/myapp.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS `myapp` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `myapp`;
+
+CREATE TABLE `data` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `a` varchar(255) COLLATE utf8_bin NOT NULL,
+    `b` varchar(255) COLLATE utf8_bin NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin
+AUTO_INCREMENT=1 ;
