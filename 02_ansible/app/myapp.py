c5117931f9a51d6925bc2ba61020c2f6f88f0ced
Pernat1y <1ns@pm.me>
2021-11-29 14:20:30 +0200

    Add application files

---
+++ 02_ansible/app/myapp.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+
+from http.server import BaseHTTPRequestHandler
+from http.server import HTTPServer
+try:
+    import pymysql.cursors
+except ImportError:
+    print('I need MySQLdb extension. Exiting.')
+    exit()
+
+
+# DB options
+db_host = '127.0.0.1'
+db_user = 'root'
+db_pass = 'root'
+db_name = 'myapp'
+
+timeout = 10
+listen_host = '0.0.0.0'
+listen_port = 8080
+
+# Connect to the database
+def db_connect():
+    connection = pymysql.connect(host=db_host,
+                                 user=db_user,
+                                 password=db_pass,
+                                 database=db_name,
+                                 cursorclass=pymysql.cursors.DictCursor)
+    return connection
+
+def create_test_data():
+    connection = db_connect()
+    with connection.cursor() as cursor:
+        # Create a new record
+        sql = "INSERT INTO `data` (`a`, `b`) VALUES (%s, %s)"
+        cursor.execute(sql, ('testing123', 'abc123'))
+
+    # connection is not autocommit by default. So you must commit to save
+    # your changes.
+    connection.commit()
+
+def query_db():
+    connection = db_connect()
+    with connection.cursor() as cursor:
+        # Read a single record
+        sql = "SELECT `a` FROM `data` WHERE `id`=1"
+        cursor.execute(sql)
+        result = cursor.fetchone()
+        return result
+
+class HttpGetHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # Query DB
+        result = query_db()
+
+        # Send results
+        if result:
+            print(f'Got results from DB query: {result}')
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(str(result).encode())
+        else:
+            self.send_response(500)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write('Something went wrong while querying backend DB.'.encode())
+
+def run(server_class=HTTPServer, handler_class=HttpGetHandler):
+    server_address = ('', listen_port)
+    httpd = server_class(server_address, handler_class)
+    try:
+        print(f'Listening *:{listen_port}')
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        httpd.server_close()
+
+
+create_test_data()  # Create test db with data
+run()  # Start server
---
+++ 02_ansible/app/readme.md
@@ -0,0 +1,14 @@
+# About
+Simple web app in Python to query DB and return REST
+
+# Usage
+Before running script, you will need to set DB connection properties:
+```db_host = '127.0.0.1'
+db_user = 'root'
+db_pass = 'root'
+db_name = 'myapp'```
+
+Place 'myapp.service' in '/usr/lib/systemd/system' directory and run
+```systemctl daemon-reload
+systemctl start myapp```
+
