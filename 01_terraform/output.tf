3a3f0122176e3d1c94c9da6757913ce3f398a652
Pernat1y <1ns@pm.me>
2021-11-30 15:22:31 +0200

    Update app url in nginx.conf; update sql permissions

--- 01_terraform/output.tf
+++ 01_terraform/output.tf
@@ -6,7 +6,6 @@ output "web_server_private_ip" {
   value = aws_instance.web-server-instance.private_ip
 }
 
-# Just to check - application server should not have a public IP
 output "app_server_public_ip" {
   value = aws_instance.application-server-instance.public_ip
 }
@@ -14,7 +13,6 @@ output "app_server_private_ip" {
   value = aws_instance.application-server-instance.private_ip
 }
 
-# Just to check - database server should not have a public IP
 output "db_server_public_ip" {
   value = aws_instance.database-server-instance.public_ip
 }
